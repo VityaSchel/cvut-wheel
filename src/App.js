3ee024537f51a523c3d672b7ba3c031fbf2fd4c3
Viktor <vityaschel@utidteam.com>
2021-06-13 23:09:05 +0400

    relative paths

--- src/App.js
+++ src/App.js
@@ -1,10 +1,7 @@
 import React from 'react'
 import './App.css'
 import Button from '@material-ui/core/Button'
-// import {arc, pie} from 'd3-shape'
 import { Random } from "random-js"
-// import WheelComponent from 'react-wheel-of-prizes'
-// import 'react-wheel-of-prizes/dist/index.css'
 import Wheel from './Wheel'
 
 function App() {
@@ -61,80 +58,9 @@ function App() {
           <Button variant='contained' color='primary' disabled={disabled} onClick={handleStart}>Крутить!</Button>
         </div>
       </div>
-      <img src="/czu.png" className="fullscreen" is-shown={shown.toString()} alt="До встречи в ČZU"/>
+      <img src="czu.png" className="fullscreen" is-shown={shown.toString()} alt="До встречи в ČZU"/>
     </>
   )
 }
-
-// function Wheel() {
-//   const [svgs, setSvgs] = React.useState(<></>)
-//
-//   React.useEffect(() => {
-//     const data = new Array(/*new Random().integer(3, 12)*2*/ 12).fill(1)
-//     const arcs = pie()(data)
-//
-//     const arcToSvg = arcValues => arc()({
-//       innerRadius: 0,
-//       outerRadius: 100,
-//       startAngle: arcValues.startAngle,
-//       endAngle: arcValues.endAngle
-//     })
-//
-//     let i = -1;
-//
-//     const svgToDocument = svgData => {
-//       i++
-//       return (
-//         <SvgPart
-//           pathData={svgData}
-//           czu={String(i===0)}
-//           even={String(i%2===0)}
-//           len={data.length}
-//           index={i}
-//         />
-//       )
-//     }
-//
-//     let arcsSvgs = arcs.map(arcToSvg)
-//     const half = Math.ceil(arcsSvgs.length / 2);
-//
-//     const firstHalf = arcsSvgs.slice(0, half)
-//     const secondHalf = arcsSvgs.slice(-half)
-//     let arcsData1s = firstHalf.map(svgToDocument)
-//     let arcsData2nd = secondHalf.map(svgToDocument)
-//     setSvgs(<><div className="firstHalf">{arcsData1s}</div><div className="secondHalf">{arcsData2nd}</div></>)
-//   }, [])
-//
-//   return (
-//     <div className="wheel-outer">
-//       {svgs}
-//     </div>
-//   )
-// }
-//
-// function SvgPart(props) {
-//   const pathRef = React.useRef()
-//   const textRef = React.useRef()
-//
-//   React.useEffect(() => {
-//     if(pathRef && textRef) {
-//       let bbox = pathRef.current.getBBox()
-//       let x = bbox.x //bbox.x / 2
-//       let y = bbox.y // bbox.y-bbox.height
-//       let deg = 360/props.len*props.index
-//       deg = deg - 90;
-//       textRef.current.style.transform = 'translate('+props.label[0]+'px, '+props.label[1]+'px) rotate('+deg+'deg)'
-//     }
-//   }, [pathRef, textRef])
-//
-//   return (
-//     <div className="wheel-part-outer">
-//       <svg className="wheel-part" {...props}>
-//         <path d={props.pathData} ref={pathRef}></path>
-//       </svg>
-//       <span ref={textRef}>{props.czu ? 'ČZU' : (props.even ? 'FIT' : 'FEL')}</span>
-//     </div>
-//   )
-// }
 
 export default App;
